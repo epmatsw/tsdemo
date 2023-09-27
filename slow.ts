cbdc83a7808a8e9b42d82915f43adb74d031c9a5
Will Stamper <wstamper@apple.com>
2023-09-27 12:32:49 -0600

    Initial commit

---
+++ slow.ts
@@ -0,0 +1,17 @@
+import type { SpecificString, SmallerString } from "./bigstrings";
+
+const thing = {} as any as Record<SpecificString, string>;
+const isSpecificString = (key: string): key is SpecificString => {
+	return key in thing;
+};
+
+const fallback = function (
+	base: SpecificString | SmallerString,
+): SpecificString {
+
+	if (isSpecificString(base)) {
+		return base as SpecificString;
+	}
+
+  return base as any;
+};
